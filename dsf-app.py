b55066d4b276d85e74041ce19a6ba1c8f7e93254
kamalcoeb1984 <165362615+kamalcoeb1984@users.noreply.github.com>
2024-09-08 19:26:07 +0530

    pdf reader

--- dsf-app.py
+++ dsf-app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import pdfReader
+
+def get_pdf_data(pdf_data):
+  pdf_txt = ""
+  for pdf in pdf_data:
+    pdf_rdr = pdfReader(pdf)
+    for pg in pdf_rdr.pages:
+      pdf_txt += pg.extract_text()
+  return pdf_txt
+
 
 def main():
   st.write("Biits Project - Dailogue System Framework!")
@@ -7,8 +18,17 @@ def main():
 
   with st.sidebar:
     st.subheader("Internal Documents :pdf:")
-    st.file_uploader("Upload PDFs and click 'Process'")
-    st.button("Process")    
+    pdf_data = st.file_uploader("Upload PDFs and click 'Process'", accept_multiple_files = True)
+    
+    if st.button("Process"):
+
+      with st.spinner("File processing"):
+        #get pdf text
+        raw_data = get_pdf_data(pdf_data)
+        st.write(raw_data)
+        #get text chunks
+
+        #create vector store
 
 if __name__ == "__main__":
   main()
