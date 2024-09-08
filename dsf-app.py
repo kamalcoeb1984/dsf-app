58edd13e1a468b53d012af42e053cf68ee44a5c4
kamalcoeb1984 <165362615+kamalcoeb1984@users.noreply.github.com>
2024-09-08 14:49:58 +0530

    Update dsf-app.py

--- dsf-app.py
+++ dsf-app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
 def main():
-  st.write("Hello World Test!")
+  st.write("Biits Project - Dailogue System Framework!")
+  st.header("LLM using personal PDF documents :books:")
+  st.text_input("Ask your queries:")
+
+  with st.sidebar:
+    st.subheader("Internal Documents")
+    st.file_uploader("Upload PDFs and click 'Process'")
+    st.button("Process")    
 
 if __name__ == "__main__":
   main()
