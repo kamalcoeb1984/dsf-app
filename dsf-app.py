3fc983733d36539629c6853e74497cce77492aef
kamalcoeb1984 <165362615+kamalcoeb1984@users.noreply.github.com>
2024-09-11 12:35:00 +0530

    Update dsf-app.py
    Signed-off-by: kamalcoeb1984 <165362615+kamalcoeb1984@users.noreply.github.com>

--- dsf-app.py
+++ dsf-app.py
@@ -5,6 +5,11 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings 
 from langchain.vectorstores import FAISS
 import os
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 # os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
@@ -28,19 +33,55 @@ def get_txt_chunks(raw_data):
   return chunks_txt
 
 def get_vectorstore(txt_chunks):
-  # embeddings = OpenAIEmbeddings(openai_api_key = os.environ["OPENAI_API_KEY"])
-  # embeddings = OpenAIEmbeddings()
-  embeddings = HuggingFaceInstructEmbeddings(model_name = "hkunlp/instructor-xl")
-  vector_store = FAISS.from_texts(texts = txt_chunks, embedding = embeddings)
-  return vector_store
+  embeddings = OpenAIEmbeddings()
+  # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+  vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+  return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
   load_dotenv()
   st.write("Biits Project - Dailogue System Framework!")
+  
   st.header("LLM using personal PDF documents :books:")
   st.text_input("Ask your queries:")
+  st.write(css, unsafe_allow_html=True)
 
+  if "conversation" not in st.session_state:
+      st.session_state.conversation = None
+  if "chat_history" not in st.session_state:
+      st.session_state.chat_history = None
+
+  st.header("Chat with multiple PDFs :books:")
+  user_question = st.text_input("Ask a question about your documents:")
+  if user_question:
+      handle_userinput(user_question)
+    
   with st.sidebar:
     st.subheader("Internal Documents!!!")
     pdf_data = st.file_uploader("Upload PDFs and click 'Process'", accept_multiple_files = True)
@@ -56,6 +97,9 @@ def main():
         
         #create vector store
         vStore = get_vectorstore(txt_chunks)
+
+        # create conversation chain
+        st.session_state.conversation = get_conversation_chain(vectorstore)
 
 
 if __name__ == "__main__":
