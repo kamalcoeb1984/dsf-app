930bc4b2f0ea0db3bdd62530ef6c3c36e6a4bbe3
kamalcoeb1984 <165362615+kamalcoeb1984@users.noreply.github.com>
2024-09-20 10:50:30 +0530

    error handling

--- dsf-app.py
+++ dsf-app.py
@@ -76,46 +76,47 @@ def handle_userinput(user_question):
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
-  load_dotenv()
-  os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
-  os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
-  os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
-  os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
-  
-  
-  st.write("Biits Project - Dailogue System Framework!")
-  
-  st.write(css, unsafe_allow_html=True)
-
-  if "conversation" not in st.session_state:
-      st.session_state.conversation = None
-  if "chat_history" not in st.session_state:
-      st.session_state.chat_history = None
-
-  st.header("LLM using personal PDF documents :books:")
-  user_question = st.text_input("Ask your queries:")
-  if user_question:
-      handle_userinput(user_question)
+  try:
+    load_dotenv()
+    os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+    os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+    os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
     
-  with st.sidebar:
-    st.subheader("Internal Documents!!!")
-    pdf_data = st.file_uploader("Upload PDFs and click 'Process'", accept_multiple_files = True)
     
-    if st.button("Process"):
+    st.header("Biits Project - Dailogue System Framework!")
+    
+    st.write(css, unsafe_allow_html=True)
 
-      with st.spinner("File processing"):
-        #get pdf text
-        raw_data = get_pdf_data(pdf_data)
-        
-        #get text chunks
-        txt_chunks = get_txt_chunks(raw_data)
-        
-        #create vector store
-        vStore = get_vectorstore(txt_chunks)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
-        # create conversation chain
-        st.session_state.conversation = get_conversation_chain(vStore)
+    st.write("LLM using personal PDF documents :books:")
+    user_question = st.text_input("Ask your queries:")
+    if user_question:
+        handle_userinput(user_question)
+      
+    with st.sidebar:
+      st.subheader("Internal Documents!!!")
+      pdf_data = st.file_uploader("Upload PDFs and click 'Process'", accept_multiple_files = True)
+      
+      if st.button("Process"):
 
+        with st.spinner("File processing"):
+          #get pdf text
+          raw_data = get_pdf_data(pdf_data)
+          
+          #get text chunks
+          txt_chunks = get_txt_chunks(raw_data)
+          
+          #create vector store
+          vStore = get_vectorstore(txt_chunks)
 
+          # create conversation chain
+          st.session_state.conversation = get_conversation_chain(vStore)
+  except Exception as e: 
+    st.write('Oops somthing is wrong. Please see below for issue: \n', e)
 if __name__ == "__main__":
   main()
